a7435655cbd405295148f5565f5893bd13cd812c
delphine abouab <delphine.abouab@outlook.fr>
2023-05-15 17:20:54 +0200

    Components & HTML created

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <header>
+      <div>
+        <img src="src/assets/react.png" alt="react logo" className="header-logo" />
+        <h2>ReactFacts</h2>
+      </div>
+      <h3>React Course - Project 1</h3>
+
+    </header>
+  )
+}
+
+export default Navbar
